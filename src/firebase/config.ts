d2d1e2dd4bc26427c75ca99a091ae14500c7f13d
Pablo Fabricio García Flores <PABLOFGARCIAF@gmail.com>
2026-03-08 06:26:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5218899653-d333b",
+  "appId": "1:534626091746:web:6e3e901a6138a1665754bb",
+  "apiKey": "AIzaSyDG0UPVLgFfsZWvCqByvdXc3_daDbpKLzo",
+  "authDomain": "studio-5218899653-d333b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "534626091746"
+};
