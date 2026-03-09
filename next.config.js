fc82976dce946db763cb4af9b06df7ee2805c394
Pablo Fabricio García Flores <PABLOFGARCIAF@gmail.com>
2026-03-09 21:43:23 +0000

    sigue desconectado "admin:1 Access to manifest at 'https://6000-firebase

--- next.config.js
+++ next.config.js
@@ -31,6 +31,7 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // Apply these headers to all routes in your application.
         source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
@@ -38,24 +39,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
-      },
-      {
-        source: '/manifest.json',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, content-type, Authorization',
-          },
-        ],
-      },
+      }
     ];
   },
 };
