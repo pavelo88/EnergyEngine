6a92952617718efa3fca4df27bb384ebe5b340cd
Pablo Fabricio García Flores <PABLOFGARCIAF@gmail.com>
2026-03-09 22:58:11 +0000

    [2026-03-09T22:07:03.252Z]  @firebase/firestore: Firestore (11.9.0): ena

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -20,17 +20,17 @@ export function initializeFirebase() {
 
   const firestore = getFirestore(firebaseApp);
   
-  enableMultiTabIndexedDbPersistence(firestore).catch((err) => {
-    if (err.code == 'failed-precondition') {
-        // Multiple tabs open, persistence can only be enabled
-        // in one tab at a a time.
-        // ...
-    } else if (err.code == 'unimplemented') {
-        // The current browser does not support all of the
-        // features required to enable persistence
-        // ...
-    }
-  });
+  // enableMultiTabIndexedDbPersistence(firestore).catch((err) => {
+  //   if (err.code == 'failed-precondition') {
+  //       // Multiple tabs open, persistence can only be enabled
+  //       // in one tab at a a time.
+  //       // ...
+  //   } else if (err.code == 'unimplemented') {
+  //       // The current browser does not support all of the
+  //       // features required to enable persistence
+  //       // ...
+  //   }
+  // });
 
   return {
     firebaseApp,
