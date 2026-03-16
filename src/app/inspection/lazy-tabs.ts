bc7a9fc8e74fa747e96e83a29b508de0046561bd
pavelo88 <pablofgarciaf@gmail.com>
2026-03-16 05:45:27 -0500

    Offline2

--- src/app/inspection/lazy-tabs.ts
+++ src/app/inspection/lazy-tabs.ts
@@ -1,35 +1,20 @@
-import React from 'react';
-
-// --- TABS DEL MENÚ PRINCIPAL ---
-export const TasksTabLazy = React.lazy(() => 
-  import('./components/HistoryTab').then(module => ({ default: module.default }))
-);
-export const RegistroGastoForm = React.lazy(() => 
-  import('./components/RegistroGastoForm').then(module => ({ default: module.default }))
-);
-export const ProfileTabLazy = React.lazy(() => 
-  import('./components/ProfileTab').then(module => ({ default: module.default }))
-);
+﻿import HistoryTab from './components/HistoryTab';
+import RegistroGastoForm from './components/RegistroGastoForm';
+import ProfileTab from './components/ProfileTab';
 
 // --- FORMULARIOS DE INSPECCIÓN ---
-export const HojaTrabajoFormLazy = React.lazy(() =>
-  import('./components/forms/HojaTrabajoForm').then(module => ({ default: module.default }))
-);
+import HojaTrabajoForm from './components/forms/HojaTrabajoForm';
+import InformeTecnicoForm from './components/forms/InformeTecnicoForm';
+import InformeRevisionForm from './components/forms/InformeRevisionForm';
+import InformeSimplificadoForm from './components/forms/InformeSimplificadoForm';
+import RevisionBasicaForm from './components/forms/RevisionBasicaForm';
 
-// CORRECCIÓN 1: Ahora apunta correctamente a InformeTecnicoForm
-export const InformeTecnicoFormLazy = React.lazy(() =>
-  import('./components/forms/InformeTecnicoForm').then(module => ({ default: module.default }))
-);
+export { HistoryTab as TasksTabLazy };
+export { RegistroGastoForm };
+export { ProfileTab as ProfileTabLazy };
 
-export const InformeRevisionFormLazy = React.lazy(() =>
-  import('./components/forms/InformeRevisionForm').then(module => ({ default: module.default }))
-);
-
-export const InformeSimplificadoFormLazy = React.lazy(() =>
-  import('./components/forms/InformeSimplificadoForm').then(module => ({ default: module.default }))
-);
-
-// CORRECCIÓN 2: Se agregó la Revisión Básica que faltaba en el mapeo
-export const RevisionBasicaFormLazy = React.lazy(() =>
-  import('./components/forms/RevisionBasicaForm').then(module => ({ default: module.default }))
-);
+export { HojaTrabajoForm as HojaTrabajoFormLazy };
+export { InformeTecnicoForm as InformeTecnicoFormLazy };
+export { InformeRevisionForm as InformeRevisionFormLazy };
+export { InformeSimplificadoForm as InformeSimplificadoFormLazy };
+export { RevisionBasicaForm as RevisionBasicaFormLazy };
